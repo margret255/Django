ede38256001b568dd7dafba5b30bc990e24af162
margret255 <wanguimargret18@gmail.com>
2026-02-19 18:09:07 +0300

    Fix AttributeError in members view and configure homepage URL

--- members/views.py
+++ members/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
-   template=loader.get_template('home.html')
-   return HttpResponse(template.render())
+      mymembers=Member.objects.all().values()
+      template=loader.get_template('all_members.html')
+      context={
+            'mymembers': mymembers,
+      }
+      return HttpResponse(template.render(context,request))
+
+def details(request,id):
+      member=Member.objects.get(id=id)
+      template=loader.get_template('details.html')
+      context={
+            'member': member,
+      }
+      return HttpResponse(template.render(context,request))
